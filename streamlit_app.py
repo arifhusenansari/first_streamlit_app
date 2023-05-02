72e5540f6578f6d82242adf0d9323166e8a3fc62
Arifhusen Ansari <ansariarifhusen@gmail.com>
2023-05-02 07:26:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My parents New Health Dinner')
+streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omaga 3 & Blueberry Oatmeal')
+streamlit.text('Kale Spinach & Rocket Smoothie')
+streamlit.text('Hard bolied Free Range EGG')
